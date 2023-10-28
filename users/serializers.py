31587d79c8fc1a45bf9bb63efc89d89cfe4368ae
lordace-coder <chiderapatrick@gmail.com>
2023-10-28 08:21:52 +0000

    web based updates

--- users/serializers.py
+++ users/serializers.py
@@ -14,14 +14,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username','email','password','is_staff']
-    
+
     def create(self, validated_data:dict):
         password = validated_data.pop('password')
         user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
         return user
-    
+
     def get_is_staff(self,obj:User):
         return obj.is_staff
 
@@ -29,7 +29,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source = 'user.username',read_only=True)
+    email = serializers.CharField(source = 'user.email',read_only=True)
     posts = serializers.SerializerMethodField()
+    post_count = serializers.SerializerMethodField()
+    perc_posts = serializers.SerializerMethodField()
     class Meta:
         model = UserProfile
         fields = (
@@ -38,9 +41,22 @@ class UserProfileSerializer(serializers.ModelSerializer):
             "image",
             "mobile",
             "address",
-            "posts"
+            "posts",
+            "email",
+            "post_count",
+            "perc_posts",
         )
-        
+
+    def get_post_count(self,obj):
+        posts = obj.get_posts_by_user()
+        count= posts.count()
+        return count
+
+    def get_perc_posts(self,obj):
+        posts = obj.get_posts_by_user()
+        count= posts.count()
+        perc = (count/150) *100
+        return perc
     def get_posts(self,obj):
         posts = obj.get_posts_by_user()
         return  UserProfilePostListSerializers(posts,many=True).data
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView,RetrieveAPIView
 from rest_framework.response import Response
 
 from posts.mixins import StaffEditOnly
@@ -27,7 +27,7 @@ def user_info(request):
 class UserProfileApiView(StaffEditOnly,RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
-    
+
     def get(self, request, *args, **kwargs):
         obj = self.get_object()
 
@@ -35,11 +35,19 @@ class UserProfileApiView(StaffEditOnly,RetrieveUpdateAPIView):
             return Response(status=401,data={"error":"for staff users only"})
         if request.user == obj.user or request.user.is_superuser:
            return super().get(request, *args, **kwargs)
-        return Response(status=400,data={"error":"permission denied"})    
-    
+        return Response(status=400,data={"error":"permission denied"})
+
     def get_object(self):
         queryset = self.get_queryset()
         user =self.request.user
         obj,created = queryset.get_or_create(user = user)
 
         return obj
+
+class UserProfileVisitorsView(RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
+
+    def get_object(self):
+        name = self.kwargs.get("author")
+        self.get_queryset().filter(
